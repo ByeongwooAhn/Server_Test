e824e843167d5ce98e466ece0f66e9e72dc27080
abyungwoo <abyungwoo@gmail.com>
2022-11-18 21:32:10 +0900

    텍스트 박스를 이용하여 서버에 데이터를 저장하기 성공.

--- Server_Test/Insert_Test/Form1.Designer.cs
+++ Server_Test/Insert_Test/Form1.Designer.cs
@@ -30,73 +30,121 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 13);
+            this.textBox1.Location = new System.Drawing.Point(30, 252);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.Size = new System.Drawing.Size(88, 21);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(34, 44);
+            this.textBox2.Location = new System.Drawing.Point(30, 277);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.Size = new System.Drawing.Size(88, 21);
             this.textBox2.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(34, 76);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 40);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(34, 122);
+            this.textBox3.Location = new System.Drawing.Point(30, 95);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.Size = new System.Drawing.Size(88, 22);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(34, 153);
+            this.textBox4.Location = new System.Drawing.Point(30, 121);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.Size = new System.Drawing.Size(88, 22);
             this.textBox4.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(34, 184);
+            this.button2.Location = new System.Drawing.Point(30, 147);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.Size = new System.Drawing.Size(88, 32);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 61);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(166, 147);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(325, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(363, 338);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 302);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Sign Up";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,10 +154,13 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Server_Test/Insert_Test/Form1.cs
+++ Server_Test/Insert_Test/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows.Forms;
 using MySqlCommand = MySql.Data.MySqlClient.MySqlCommand;
 using MySqlConnection = MySql.Data.MySqlClient.MySqlConnection;
@@ -8,15 +10,15 @@ namespace Insert_Test
 {
     public partial class Form1 : Form
     {
+        string connstr = "Server = 127.0.0.1;Port=3306;Database=member;Uid=root;Pwd=root";
         public Form1()
         {
             InitializeComponent();
         }
 
-        public void InsertDB()
+        public void Signup()
         {
-            string connstr = "Server = 127.0.0.1;Port=3306;Database=member;Uid=root;Pwd=root";
-            string sql = "Insert Into member_name (이름, 성별) values ('test4', '여장남장')";
+            string sql = "Insert Into member_name (이름, 성별) values ('" + textBox1.Text + "', " + textBox2.Text + ")";
 
             using (MySqlConnection conn = new MySqlConnection(connstr))
             {
@@ -26,17 +28,37 @@ namespace Insert_Test
                     MySqlCommand cmd = new MySqlCommand(sql, conn);
                     cmd.ExecuteNonQuery();
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) { }
+            }
+        }
 
-                }
+        public void Login()
+        {
+            string sql = "select 이름, 성별 from member_name" ;
+            MySqlConnection conn = new MySqlConnection(connstr);
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                textBox3.Text = reader["이름"].ToString();
+                textBox4.Text = reader["성별"].ToString();
             }
 
         }
 
         public void DeleteDB()
         {
-            string connstr = "Server = 127.0.0.1;Port=3306;Database=member;Uid=root;Pwd=root";
             string sql = "Delete From member_name Where 이름 = 'test4'";
 
             using (MySqlConnection conn = new MySqlConnection(connstr))
@@ -54,14 +76,80 @@ namespace Insert_Test
             }
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
+        public void UpdateDB()
         {
-            InsertDB();
+            string sql = "Update member_name Set 이름 ='홍길동' where 이름 = 'test4'";
+
+            using (MySqlConnection conn = new MySqlConnection(connstr))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        public DataSet SelectDB()
+        {
+            string sql = "select * from member_name";
+            DataSet ds = new DataSet();
+
+            using (MySqlConnection conn = new MySqlConnection(connstr))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+                using (MySqlConnection conn = new MySqlConnection(connstr))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+                    da.Fill(ds);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return ds;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            DeleteDB();
+            Login();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataSet ds;
+            ds = SelectDB();
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Signup();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            UpdateDB();
         }
     }
 }
